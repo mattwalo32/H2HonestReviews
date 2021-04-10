4e65800f6d8c464091d89c767ff5f042d43e2e43
Shoshanna Perchanok <sp26@illinois.edu>
2021-04-09 23:37:10 -0500

    static UI funtional

--- frontend/src/WaterGallery.js
+++ frontend/src/WaterGallery.js
@@ -4,8 +4,11 @@ import { Layout, Menu} from "antd";
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
+const useStylesGrid = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
@@ -16,16 +19,23 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const useStylesSlider = makeStyles({
+  root: {
+    width: 300,
+  },
+});
+
 
 function WaterGallery() {
-  const classes = useStyles();
+  const gridClasses = useStylesGrid();
+  const sliderClasses = useStylesSlider();
 
   const [waterList, setWaterList] = useState([]);
   const [waterListUpdated, setWaterListUpdated] = useState(false);
+
+  const [sliderValue, setSliderValue] = React.useState(30);
   
-  const dummyData = [{waterName: 'Water', waterManufacturer: 'Manufacturer', 
-                      waterAvgRating: 5, waterAvgTaste: 5, waterAvgPrice: 5, 
-                      waterAvgMouthFeel: 5, waterAvgPortability: 5, waterAvgPackQual: 5}]
+  const dummyData = [{waterName: "Water", waterManufacturer: "Manufacturer", waterAvgRating: 5}]
   /*                    
   useEffect(() => {
     Axios.get('http://localhost:3002/api/get_waters').then((response) => {
@@ -36,7 +46,6 @@ function WaterGallery() {
   const updateWaterList = () => {
     console.log(waterListUpdated)
     if (!waterListUpdated) {
-      console.log("here!")
       setWaterListUpdated(true);
       setWaterList(waterList.concat(dummyData));
     }
@@ -46,32 +55,44 @@ function WaterGallery() {
     setWaterListUpdated(false);
   }
 
+  const handleSliderChange = (event, newValue) => {
+    setSliderValue(newValue);
+  };
+
   return (
     <div className="WaterGallery">
       <h1> Waters </h1>
       {updateWaterList()}
       <div className="form">
-        <div className={classes.root}>
+        <Button variant="contained" onClick={setWaterListNeedsUpdate}> Update</Button>
+        <br></br>
+        <Button variant="contained" onClick={setWaterListNeedsUpdate}> Show Favorite Waters of Users You Follow</Button>
+        <br></br>
+        <div className={sliderClasses.root}>
+          <Typography id="continuous-slider" gutterBottom>
+            FILTER BY RATING
+          </Typography>
+          <Slider value={sliderValue} onChange={handleSliderChange} valueLabelDisplay="auto"
+                  step={0.1}
+                  marks
+                  min={0}
+                  max={5} />
+        </div>
+        <div className={gridClasses.root}>
           <Grid container spacing={3}>
-            {waterList.map((index, val) => {
+            {waterList.map((val, index) => {
               return (
                 <Grid key={index} item>
-                  <Paper className={classes.paper}>
+                  <Paper className={gridClasses.paper}>
                     <h1>Water Name: {val.waterName} </h1>
                     <p>Manufacturer: {val.waterManufacturer}</p>
                     <p>Average Rating: {val.waterAvgRating}/5</p>
-                    <p>Average Taste: {val.waterAvgTaste}/5</p>
-                    <p>Average Price: {val.waterAvgPrice}/5</p>
-                    <p>Average Mouth Feel: {val.waterAvgMouthFeel}/5</p>
-                    <p>Average Portability: {val.waterAvgPortability}/5</p>
-                    <p>Average Packaging Quality: {val.waterAvgPackQual}/5</p>
                   </Paper>
                 </Grid>
               );
             })}
           </Grid>
         </div>
-        <button onClick={setWaterListNeedsUpdate}> Update</button>
       </div>
     </div>
   );
